4d9cb4adea0165a1cacae3ee5a0d16cff50d1f39
ShurkaWake <a.a.prosud@gmail.com>
2022-11-27 10:33:14 +0200

    Added array generation method and global usings

---
+++ ParPr_Lb2/Utils.cs
@@ -0,0 +1,18 @@
+﻿namespace ParPr_Lb2;
+
+internal static class Utils
+{
+    const int upperGenBound = 128;
+    const int lowerGenBound = -128;
+
+    public static int[] GetArray(int length)
+    {
+        int[] array = new int[length];
+        for(int i = 0; i < length; i++)
+        {
+            array[i] = RandomNumberGenerator.GetInt32(
+                lowerGenBound, upperGenBound);
+        }
+        return array;
+    }
+}
