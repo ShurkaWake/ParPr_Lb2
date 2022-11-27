085cb29a725154b345611e384836e6c0c8620266
ShurkaWake <a.a.prosud@gmail.com>
2022-11-27 20:00:01 +0200

    Task2

--- ParPr_Lb2/Program.cs
+++ ParPr_Lb2/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using ParPr_Lb2;
 
-Task1.Show();
+Task2.Show();
--- ParPr_Lb2/Task2.cs
+++ ParPr_Lb2/Task2.cs
@@ -2,5 +2,66 @@
 
 public static class Task2
 {
+    public static void Show()
+    {
+        int arrayLength = 1 << 10;
 
+        Console.WriteLine();
+        Console.WriteLine("~#~#~#~#~ Task2 ~#~#~#~#~");
+        Console.WriteLine();
+
+        int[] arr1 = Utils.GetArray(arrayLength);
+        int[] arr2 = arr1.Clone() as int[];
+        var notOptTime = Utils.GetTime(() => BubbleSort(ref arr1))
+            / (double)TimeSpan.TicksPerMillisecond; ;
+        var optTime = Utils.GetTime(() => BubbleSort(ref arr2))
+            / (double)TimeSpan.TicksPerMillisecond; ;
+
+        Console.WriteLine($"Length: {arrayLength} " +
+            $"| Not optimized time: {notOptTime:F4} ms " +
+            $"| Optimized time {optTime:F4} ms");
+
+        Console.WriteLine();
+        Console.WriteLine("~#~#~#~#~ Task2 ~#~#~#~#~");
+        Console.WriteLine();
+    }
+
+    private static void BubbleSort(ref int[] arr)
+    {
+        for (int i = 0; i < arr.Length - 1; i++)
+        {
+            for (int j = i + 1; j < arr.Length; j++) 
+            { 
+                if (arr[i] > arr[j])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
+    }
+
+    private static void BubbleSortOtpimized(ref int[] arr)
+    {
+        for (int i = 0; i < arr.Length - 1; i++)
+        {
+            bool flag = false;
+            for (int j = i + 1; j < arr.Length; j++)
+            {
+                if (arr[i] > arr[j])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                    flag = true;
+                }
+            }
+
+            if (!flag)
+            {
+                break;
+            }
+        }
+    }
 }
